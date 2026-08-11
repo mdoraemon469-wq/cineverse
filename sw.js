0753e8c316aef8bbffed7354da29b9ce3b6000a7
Md Mushfiqur <mdoraemon469@gmail.com>
2026-08-11 20:06:40 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cineverse-v1';
+const ASSETS = [
+  '/cineverse/',
+  '/cineverse/index.html',
+  '/cineverse/manifest.json',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
